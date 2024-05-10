98e77962165e38dfc20354d6526003e85378d5be
sathwikreddy122 <venkatasathwik12604@gmail.com>
2024-05-10 12:51:29 +0530

    landing page completed

---
+++ productPage.js
@@ -0,0 +1,11 @@
+import { selectedIndex } from "./sa_index";
+console.log(selectedIndex);
+let imageAndText = document.querySelector(".imageAndText");
+let imageDiv = document.createElement("div");
+
+
+imageAndText.classList.add("imageAndText");
+
+
+let productImage = document.createElement("img");
+imageDiv.append(productImage);
